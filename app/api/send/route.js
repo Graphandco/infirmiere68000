d5a1f922f33dc9ba696a273131815a1dd41bbd45
Régis Daum <contact@graphandco.com>
2025-12-23 20:08:58 +0100

    Add docker

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,9 +1,8 @@
 import { EmailTemplate } from "@/components/email-template";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function POST(req) {
+   const resend = new Resend(process.env.RESEND_API_KEY);
    try {
       const body = await req.json();
 
